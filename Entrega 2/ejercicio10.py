f889c16d57f5ac04d2a374992f45579fd5c4baf3
Martin Martinez <martimartineez@gmail.com>
2024-04-06 17:27:24 -0300

    add funciones.py

---
+++ Entrega 2/ejercicio10.py
@@ -0,0 +1,22 @@
+import funciones.py
+names = """ Agustin, Yanina, Andrés, Ariadna, Bautista, CAROLINA,
+CESAR, David, Diego, Dolores, DYLAN, ELIANA, Emanuel, Fabián, Noelia,
+Francsica', FEDERICO, Fernanda, GONZALO, Nancy """
+goals = [0, 10, 4, 0, 5, 14, 0, 0, 7, 2, 1, 1, 1, 5, 6, 1, 1, 2, 0,
+11]
+goals_avoided = [0, 2, 0, 0, 5, 2, 0, 0, 1, 2, 0, 5, 5, 0, 1, 0, 2,
+3, 0, 0]
+assists = [0, 5, 1, 0, 5, 2, 0, 0, 1, 2, 1, 5, 5, 0, 1, 0, 2, 3, 1,
+0]
+
+#1
+info=generarEstructura()
+print(info)
+#2
+goleador=goleadorEs(info)
+#3
+masInfluyente(info)
+#4
+promedioGolesGeneral(info)
+#5
+promedioGolesGoleador(goleador)
